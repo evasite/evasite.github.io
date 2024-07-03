a0b92c42b7f9f6f09d791143900579a42b63206e
evasite <akshanshsecond57@gmail.com>
2024-07-03 23:44:32 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $servername="localhost";
-   $username="root";
-   $password="";
-   $dbname="evaadventures";
-   $conn=mysqli_connect($servername,$username,$password,$dbname);
-   if(!$conn)
-   echo "connection failed"
-?>
