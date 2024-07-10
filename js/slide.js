19822b91bc5d1366bc5cf430cd67fc12c31c071c
evasite <akshanshsecond57@gmail.com>
2024-07-10 18:50:03 +0530

    Add files via upload

---
+++ js/slide.js
@@ -0,0 +1,33 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+
+    const offset = -index * 100;
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function autoSlide() {
+    nextSlide();
+    setTimeout(autoSlide, 5000); // Change slide every 5 seconds
+}
+
+// Initial call to set up auto sliding
+autoSlide();
